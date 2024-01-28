df031eb39032f7cfa73ab9db5c8975e13c1a1b62
Norbaev <timnrbv@gmail.com>
2024-01-28 20:09:09 +0500

    add dotenv

--- src/db/sequelize.ts
+++ src/db/sequelize.ts
@@ -1,6 +1,12 @@
 import { Sequelize } from 'sequelize';
+import 'dotenv/config';
 
-export const sequelize = new Sequelize('store_template', 'postgres', 'root', {
-  host: 'localhost',
-  dialect: 'postgres',
-});
+export const sequelize = new Sequelize(
+  process.env.DB_DATABASENAME!,
+  process.env.DB_LOGIN!,
+  process.env.DB_PASSWORD!,
+  {
+    host: 'localhost',
+    dialect: 'postgres',
+  }
+);
